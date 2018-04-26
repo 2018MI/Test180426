e192e45aa0641cfd36de9d266854d4ba2a4c344a
orgchengpx <2819573709@qq.com>
2018-04-26 20:34:28 +0800

    工程初始化
    by chengpx

---
+++ app/src/main/java/org/chengpx/test180426/MainActivity.java
@@ -0,0 +1,14 @@
+package org.chengpx.test180426;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
